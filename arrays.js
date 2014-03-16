2319cf1614878ed3a10945ea05310e99b0f04c80
Avi <Avi@admintheweb.com>
2014-03-17 01:12:11 +0200

    first array exercise

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var index = 20, arr = [], sub = 0;
+for (var i=0; i < index; ++i){
+	if(i%2){
+		arr[i] = sub
+	} else {
+		arr[i] = index - sub++;;
+	}
+}
--- loops.js
+++
@@ -1,6 +0,0 @@
-var summary = 0;
-for(var i=10;i <= 99; i++){
-	if((i%3) && (i%7)){
-		summary += i;
-	}
-}
